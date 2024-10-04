60d1be0671a7813caa6ed4fe0d1258259c2967b2
Hao Pham <pvhao.dh14pm@gmail.com>
2024-10-04 14:20:10 +0700

    Define term and condition page

---
+++ src/pages/TermsAndConditions/index.jsx
@@ -0,0 +1,81 @@
+import './styles.scss';
+
+const TermsAndConditions = () => {
+  return (
+    <div className="terms-conditions__page">
+      <p>
+        Please read the following Agreement prior to using Shinhan Bank Canada Internet Banking Service (“Service”). The
+        Agreement contains important information, guidelines, and the terms and conditions governing access to the
+        Service. This Agreement, as amended from time to time, applies when you access or use the Service.
+      </p>
+      <p className="mt-2">
+        You hereby agree to be bound by the terms and conditions of this Agreement. When you access your accounts
+        through the Service, you do so under the terms and conditions of Personal or Business Banking Agreement. You
+        also agree to comply with all instructions we may give you connected to accessing and using the Service.
+      </p>
+      <p className="term-section-title mt-4">Terms used in this Agreement</p>
+      <p className="mt-2">“Account” means an eligible chequing or savings account;</p>
+      <p>“Agreement” means the Internet Banking Services Agreement of Shinhan Bank Canada;</p>
+      <p>
+        ““Business Day” means all days excluding Saturdays, Sundays, and statutory federal or provincial holidays in
+        Canada;
+      </p>
+      <p>
+        ““Business Day” means all days excluding Saturdays, Sundays, and statutory federal or provincial holidays in
+        Canada;
+      </p>
+      <p>
+        “Electronic Access Device” means a personal computer, telephone, cell phone, smart phone, personal digital
+        assistant or any other allowed electronic device, including wireless devices, used to access the Service;
+      </p>
+      <p>
+        “Internet Banking” means the banking services accessible by internet whereby Shinhan Bank Canada allows
+        customers to view account information and/ or make transactions online upon successful authentication;
+      </p>
+      <p>
+        “Mobile Banking Website” means the Bank website specially designed for Internet Banking access through a Mobile
+        Electronic Access Device. This Mobile Banking Website has a selective view and functionality;
+      </p>
+      <p>
+        “Password” means a confidential combination of numbers and/or letters selected by each individual customer to
+        identify themselves in order to access the Service.
+      </p>
+      <p>A temporary password will be provided for customers when first logging in.</p>
+      <p>
+        “Security Device” means an instrument with a unique combination of numbers the Bank issues to each customer at
+        the branch upon completing the Internet Banking Application. To initiate certain transactions, customers must
+        provide the numbers that correspond to the instructions given by the Service. The Shinhan Password Card used for
+        Internet Banking is an example of a security device;
+      </p>
+      <p>
+        “Security Questions” means the questions and answers customers may be required to choose and provide answers for
+        when prompted in order to confirm customers’ identities when accessing the Service from unregistered electronic
+        access device;
+      </p>
+      <p>“Service” means any product or service accessible through Internet Banking;</p>
+      <p>
+        “Third Party Service Provider” means a party retained by the Bank to act on the Bank’s behalf to provide, or to
+        assist in providing, the Service.
+      </p>
+      <p>
+        In the event that such party is located in a foreign jurisdiction, it is bound by the laws of the jurisdiction
+        in which it is located and may disclose personal information in accordance with those laws;
+      </p>
+      <p>
+        “User ID” means a combination of numbers and/ or letters that we may allow you to select. You must provide your
+        User ID with your Password to access the Internet Banking;
+      </p>
+      <p>“We,” “us,” “our,” “Bank,” and “SHBC” refer to Shinhan Bank Canada;</p>
+      <p>
+        “Website” means the website operated by SHBC or a SHBC affiliate through which customers sign in to the Internet
+        Banking Service;
+      </p>
+      <p>
+        “You” and “your(s)” refer to each person who applies to use the Service and each person who uses the Service
+        with the applicant’s authorization.
+      </p>
+    </div>
+  );
+};
+
+export default TermsAndConditions;
