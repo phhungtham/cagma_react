39c0245ff924a0a11c3321582b850190db2813e7
Hao Pham <pvhao.dh14pm@gmail.com>
2024-10-08 13:42:52 +0700

    remove unused file

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,11 +1,11 @@
 export { default as useCleanUpStore } from './useCleanUpStore';
 export { default as useClickedOutside } from './useClickedOutside';
 export { default as useComposeRefs } from './useComposeRefs';
-export { default as useFetchesOnce } from './useFetchesOnce';
 export { default as useFocus } from './useFocus';
 export { default as useHandler } from './useHandler';
 export { default as useHttpStatus } from './useHttpStatus';
-export { default as useReducers } from './useReducers';
-export { default as useSagas } from './useSagas';
 export { default as useLoginInfo } from './useLoginInfo';
 export { default as useNativeBack } from './useNativeBack';
+export { default as useReducers } from './useReducers';
+export { default as useSagas } from './useSagas';
+
