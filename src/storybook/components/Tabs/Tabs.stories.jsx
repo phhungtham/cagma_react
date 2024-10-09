0e859376792c6cc643e2be585ae205c9a22416b5
Hao Pham <pvhao.dh14pm@gmail.com>
2024-10-09 10:25:44 +0700

    remove unused component

--- src/storybook/components/Tabs/Tabs.stories.jsx
+++ src/storybook/components/Tabs/Tabs.stories.jsx
@@ -1,20 +1,11 @@
 import Tabs from '@common/components/atoms/Tabs';
-import Notification from '@common/components/organisms/Notification';
 
 export default {
   title: 'SOL CAMBODIA STORYBOOKS/Atomic Component/Tabs/Tab',
   component: Tabs,
 };
 
-const Template = args => (
-  <Tabs {...args}>
-    <Notification
-      notifyContent="Deposit of USD 100,000.00 from Maria(*8888) to *1234"
-      thumbnail=""
-      time="27.03.2022 13:05"
-    />
-  </Tabs>
-);
+const Template = args => <Tabs {...args} />;
 
 export const Tab = Template.bind({});
 Tab.args = {
