b9a60e2a755690a17450102a989d20253dcc4190
Hao Pham <pvhao.dh14pm@gmail.com>
2024-12-02 11:56:45 +0700

    fix complete icon size

--- src/pages/SignUp/components/VerifyMembershipResult/index.jsx
+++ src/pages/SignUp/components/VerifyMembershipResult/index.jsx
@@ -53,7 +53,7 @@ const VerifyMembershipResult = ({ type, onNavigateVerifyMembership }) => {
 
   return (
     <>
-      <div className="page-success">
+      <div className="page-success page-gradient">
         <div className="success__header">
           <div className="success__img">
             <img
