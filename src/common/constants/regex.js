c9dd007b3cb5effe4978e4faadd8475a941a4856
Tran Duy Minh <sgo1029673@shinhan.com>
2024-12-03 11:44:32 +0900

    Update Invalid Name Regex SignUp
    Co-authored-by: Duy Minh <tdminh334546@gmail.com>
    Reviewed-on: http://10.25.160.80:3000/shinhanglobal/cagma_react/pulls/85
    Co-authored-by: Tran Duy Minh <sgo1029673@shinhan.com>
    Co-committed-by: Tran Duy Minh <sgo1029673@shinhan.com>

--- src/common/constants/regex.js
+++ src/common/constants/regex.js
@@ -3,4 +3,4 @@ export const postalCodeNotAllowRegex = /[^0-9a-zA-Z-]/g;
 export const notAllowNumberRegex = /[^0-9]/g;
 export const notAllowNumberAlphabetRegex = /[^0-9a-zA-Z]/g;
 export const notAllowSpaceRegex = /\s+/g;
-export const invalidNameRegex = /[^0-9a-zA-Z.'-\s]/g;
+export const invalidNameRegex = /[^0-9a-zA-Z.‘’'-\s]/g;
