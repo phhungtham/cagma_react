6ffc4fda5bcce85fee413fe9b50151e6cd9ddfff
Hao Pham <pvhao.dh14pm@gmail.com>
2024-12-09 18:04:54 +0700

    apt number optional for add new card

--- src/pages/Card/AddNewCard/components/EnterNewCardInfo/schema.js
+++ src/pages/Card/AddNewCard/components/EnterNewCardInfo/schema.js
@@ -4,7 +4,7 @@ export const newCardFormSchema = Yup.object().shape({
   accountNo: Yup.string().required('Required field'),
   streetNumber: Yup.string().required('Required field'),
   streetName: Yup.string().required('Required field'),
-  aptNumber: Yup.string().required('Required field'),
+  aptNumber: Yup.string(),
   city: Yup.string().required('Required field'),
   province: Yup.string().required('Required field'),
   postalCode: Yup.string().required('Required field'),
