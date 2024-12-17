7aad1bd06a5a7ee11d31f2955157b9d8e3b792aa
Tran Duy Minh <sgo1029673@shinhan.com>
2024-12-17 10:38:51 +0900

    [CMRU-944] - Change JSON to Object when navigate to account activity
    Co-authored-by: Duy Minh <tdminh334546@gmail.com>
    Reviewed-on: http://10.25.160.80:3000/shinhanglobal/cagma_react/pulls/113
    Co-authored-by: Tran Duy Minh <sgo1029673@shinhan.com>
    Co-committed-by: Tran Duy Minh <sgo1029673@shinhan.com>

--- src/pages/Account/OpenAccount/components/OpenAccountSuccessful/index.jsx
+++ src/pages/Account/OpenAccount/components/OpenAccountSuccessful/index.jsx
@@ -25,9 +25,9 @@ const OpenAccountSuccessful = ({ openAccountInfo, productCode, dep_sjt_class, tr
   const { moveScreenNative, moveHomeNative } = useMove();
 
   const onClickViewAccount = () => {
-    const accountNumberParam = JSON.stringify({
+    const accountNumberParam = {
       lcl_acno: openedAccountNumber,
-    });
+    };
     let menuCode = '';
     if (dep_sjt_class === DepositSubjectClass.REGULAR_SAVING) {
       menuCode = MENU_CODE.ACCOUNT_ACTIVITY_BANKING;
