6f34b2b1959e6464fad6d36ec59613614a0f3793
Hao Pham <pvhao.dh14pm@gmail.com>
2024-12-30 16:29:50 +0700

    update resend email show confirm alert

--- src/pages/UpdateEmail/components/IdentityVerifyResult/components/IdentityVerifyInProgress.jsx
+++ src/pages/UpdateEmail/components/IdentityVerifyResult/components/IdentityVerifyInProgress.jsx
@@ -1,14 +1,29 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import VerifyId from '@assets/images/icon-fill-idpw-24.png';
 import StartOverIcon from '@assets/images/icon_fill_restart_24.png';
 import LoadingImg from '@assets/images/signup-spinner.png';
+import Alert from '@common/components/atoms/Alert';
 import { IconButton } from '@common/components/atoms/ButtonGroup/IconButton/IconButton';
 import { updateEmailLabels as labels } from '@common/constants/labels';
 import { UpdateEmailContext } from '@pages/UpdateEmail';
 
 const IdentityVerifyInProgress = ({ showRetryBtn, onClickResendEmail }) => {
-  const { translate: t, onRestart } = useContext(UpdateEmailContext);
+  const { translate: t, onRestart, updateEmailInfo } = useContext(UpdateEmailContext);
+  const [showResendEmailConfirmAlert, setShowResendEmailConfirmAlert] = useState(false);
+
+  const handleCloseConfirmAlert = () => {
+    setShowResendEmailConfirmAlert(false);
+  };
+
+  const handleShowConfirmAlert = () => {
+    setShowResendEmailConfirmAlert(true);
+  };
+
+  const handleClickResend = () => {
+    setShowResendEmailConfirmAlert(false);
+    onClickResendEmail();
+  };
 
   return (
     <>
@@ -35,7 +50,7 @@ const IdentityVerifyInProgress = ({ showRetryBtn, onClickResendEmail }) => {
               type="circle"
               label={t(labels.resendEmail)}
               icon={<img src={VerifyId} />}
-              onClick={onClickResendEmail}
+              onClick={handleShowConfirmAlert}
             />
             <IconButton
               size="lg"
@@ -48,6 +63,24 @@ const IdentityVerifyInProgress = ({ showRetryBtn, onClickResendEmail }) => {
           </>
         )}
       </div>
+      {showResendEmailConfirmAlert && (
+        <Alert
+          isCloseButton={false}
+          isShowAlert={showResendEmailConfirmAlert}
+          title={t(labels.emailHasAlreadySent)}
+          subtitle={t(labels.aVerificationEmailHas).replace('%1', updateEmailInfo?.email)}
+          onClose={handleCloseConfirmAlert}
+          textAlign="left"
+          firstButton={{
+            onClick: handleCloseConfirmAlert,
+            label: t(labels.confirm),
+          }}
+          secondButton={{
+            onClick: handleClickResend,
+            label: t(labels.resend2),
+          }}
+        />
+      )}
     </>
   );
 };
