5cf5cbd68fb0d6021c966fe62da7f51b6af14af0
Hao Pham <pvhao.dh14pm@gmail.com>
2025-01-16 15:32:52 +0700

    update require Uppercase enter ID

--- src/pages/SignUp/components/CreateID/schema.js
+++ src/pages/SignUp/components/CreateID/schema.js
@@ -1,5 +1,9 @@
 import * as Yup from 'yup';
 
 export const createIdFormSchema = Yup.object().shape({
-  id: Yup.string().min(6).max(20).matches(/[a-z]/).required('Required field'),
+  id: Yup.string()
+    .min(6)
+    .max(20)
+    .matches(/[a-zA-Z]/)
+    .required('Required field'),
 });
